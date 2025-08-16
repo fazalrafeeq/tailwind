5ccbe46d245991841933d7b4a0dc245f8a4d2904
Fazal Rafeeq <your_email@example.com>
2025-08-16 15:55:16 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src//*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#1DA1F2',
+      },
+    },
+  },
+  plugins: [],
+};
